0df4c95a1cf93363fc2803efe3c28bdc52e62b2a
blacksmith2000 <73566550+blacksmith2000@users.noreply.github.com>
2021-02-23 09:04:03 +0800

    Add files via upload

--- jd_fruit.js
+++ jd_fruit.js
@@ -29,10 +29,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'c144db23b6fc4e779cf1112d1e1b052d@ed1ccad2bcd940119d93c83b9dfa6fda@fc943e869edf479aa99e1ec01ac26401@fb7f2258765449c693098d163d86463d@b71b5b2efe75423997d05b8017d44077@93bf6db5b5a24707830342a20cd83c44@2dc3b315f6cf4bde8ee3fba309986c1d@d674f26ee3424a1fbcdfb74c799156b9@40037e4227b04bf6b44a8d6dc646381b@840731518a5043dea2e5c663fd6a03f0',
-  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'c144db23b6fc4e779cf1112d1e1b052d@ed1ccad2bcd940119d93c83b9dfa6fda@fc943e869edf479aa99e1ec01ac26401@fb7f2258765449c693098d163d86463d@b71b5b2efe75423997d05b8017d44077@93bf6db5b5a24707830342a20cd83c44@2dc3b315f6cf4bde8ee3fba309986c1d@d674f26ee3424a1fbcdfb74c799156b9@40037e4227b04bf6b44a8d6dc646381b@840731518a5043dea2e5c663fd6a03f0',
+   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开 '9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6','9e195230d84e405cb0b5d449555b2b6b@47185c0af3b04054a6eba856fa5f974d@46f8cdd7d0894ccaa3a387858b9c8eb2@3aada576c8e749a988b483c560e5d4ab@f418bcd262c14b8fb221afe628bcc2a6'
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
@@ -1229,7 +1226,7 @@ async function showMsg() {
   if (ctrTemp) {
     $.msg($.name, subTitle, message, option);
     if ($.isNode()) {
-      await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `${subTitle}\n${message}`);
+      //await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `${subTitle}\n${message}`);
     }
   } else {
     $.log(`\n${message}\n`);
@@ -1248,7 +1245,7 @@ function timeFormat(time) {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: "https://gitee.com/Soundantony/RandomShareCode/raw/master/JD_Fruit.json",headers:{
+    $.get({url: "https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/total/JD_Fruit.json",headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }}, async (err, resp, data) => {
       try {
@@ -1256,7 +1253,7 @@ function readShareCode() {
           console.log(`${JSON.stringify(err)}`)
           console.log(`${$.name} API请求失败，将切换为备用API`)
           console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
-          $.get({url: `https://raw.githubusercontent.com/shuyeshuye/RandomShareCode/main/JD_Fruit.json`, 'timeout': 10000},(err, resp, data)=>{
+          $.get({url: `https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/backUp/total/JD_Fruit.json`, 'timeout': 10000},(err, resp, data)=>{
           data = JSON.parse(data);})
         } else {
           if (data) {
